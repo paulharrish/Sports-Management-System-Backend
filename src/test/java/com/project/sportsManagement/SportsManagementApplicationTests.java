6431e49144569845e4b7364f47863552344d00f2
paulharrish <prasannaharrish@gmail.com>
2024-02-13 22:14:58 +0530

    Initial Commit

---
+++ src/test/java/com/project/sportsManagement/SportsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.sportsManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

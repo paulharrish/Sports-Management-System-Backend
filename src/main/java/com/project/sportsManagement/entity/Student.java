cf810f1016c6f4ec0fdd66df44e09b5c3239ccf9
paulharrish <prasannaharrish@gmail.com>
2024-02-13 22:13:20 +0530

    Initial Commit

--- src/main/java/com/project/sportsManagement/entity/Student.java
+++ src/main/java/com/project/sportsManagement/entity/Student.java
@@ -1,4 +1,11 @@
 package com.project.sportsManagement.entity;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class Student {
+
+    private Stirng first_name;
+    private String last_name;
+
 }
